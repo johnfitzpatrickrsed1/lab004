954132783dededbb7ded0c31ed672fa48c7f2e12
johnfitzpatrickrsed1 <john.fitzpatrick+rsed1@rightscale.com>
2013-01-09 16:25:20 +0000

    fixed a typo

--- cookbooks/splunk/attributes/default.rb
+++ cookbooks/splunk/attributes/default.rb
@@ -1,4 +1,4 @@
-iset[:splunk][:installdir]         = "/opt/splunk"
+set[:splunk][:installdir]         = "/opt/splunk"
 default[:splunk][:user]           = "splunk"
 default[:splunk][:installfile]    = https://rs-training-assets-student01.s3.amazonaws.com/splunk-5.0-140868-Linux-x86_64.tar
 
